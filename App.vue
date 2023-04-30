725b90cc31f528e45c149602a97ed4f7a511409c
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2023-04-30 21:53:19 +0900

    merge test
    test

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!--loginView userId="testid"/-->
+  <!--chatView userId="testid"/-->
+  <loginView Id="testid"/>
+  <!--payView userId="testid"/-->
+  <!--writePostViewView userId="testid"/-->
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import loginView from './components/loginView.vue'
+
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld,
+//   }
+// }
+
+export default {
+  name: 'App',
+  components: {
+    loginView
+  }
+}
+
+</script>
