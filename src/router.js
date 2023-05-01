2b77330f9971e628c619465273617d89986b7725
chikako_2426 <78514962+kimikitae@users.noreply.github.com>
2023-05-01 15:00:59 +0900

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,18 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router"
 
+import loginView from "./components/loginView"
+import homePageView from "./components/homePageView"
+import setlocView from "./components/setlocView"
+import userinfoView from "./components/userinfoView"
+import viewpostView from "./components/viewpostView"
+import writepostView from "./components/writepostView"
 import chatView from "./components/chatView"
 import chatListView from "./components/chatListView"
 import payView from "./components/payView"
-import Home from "./components/homePageView"
-import Login from "./components/loginView"
 
 // 라우터 설계
 const routes = [
-        {path:"/", component: Login},
-        {path:"/Home", component: Home},
-        {path:"/", component: chatListView},
+        {path: "/", component: loginView},
+        {path: "/homePageView", component: homePageView},
+        {path: "/setlocView", component: setlocView},
+        {path: "/userinfoView", component: userinfoView},
+        {path: "/viewpostView", component: viewpostView},
+        {path: "/writepostView", component: writepostView},
+        {path:"/chatListView", component: chatListView},
         {path:"/chatView", component: chatView},
         {path:"/payView", component: payView}
+]
 
 const router = createRouter({
     history: createWebHistory(),
