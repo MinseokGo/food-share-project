f2d33db4c1715879fa767b99710b91beb63a4251
chikako_2426 <78514962+kimikitae@users.noreply.github.com>
2023-05-02 15:02:31 +0900

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router"
 
-/*import LoginView from "./components/LoginView"
+import LoginView from "./components/LoginView"
 import HomePageView from "./components/HomePageView"
 import SetLocView from "./components/SetLocView"
 import UserInfoView from "./components/UserInfoView"
-import ViewPostView from "./components/ViewPostView"*/
+import ViewPostView from "./components/ViewPostView"
 import WritePostView from "./components/WritePostView"
 import ChatView from "./components/ChatView"
 import ChatListView from "./components/ChatListView"
@@ -12,14 +12,13 @@ import PayView from "./components/PayView"
 
 // 라우터 설계
 const routes = [
-        /*{path: "/", component: LoginView},
+        {path: "/", component: LoginView},
         {path: "/HomePageView", component: HomePageView},
         {path: "/SetLocView", component: SetLocView},
         {path: "/UserInfoView", component: UserInfoView},
-        {path: "/ViewPostView", component: ViewPostView},*/
-        {path:"/", component: ChatListView},
+        {path: "/ViewPostView", component: ViewPostView},
         {path: "/WritePostView", component: WritePostView},
-       // {path:"/ChatListView", component: ChatListView},
+        {path:"/ChatListView", component: ChatListView},
         {path:"/ChatView", component: ChatView},
         {path:"/PayView", component: PayView}
 ]
