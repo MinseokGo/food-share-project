76a5cf4a65ae6c41a341d50470e8fc6b979e0cdf
chikako_2426 <78514962+kimikitae@users.noreply.github.com>
2023-05-07 14:20:54 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="outside_box">
     <router-view></router-view>
   </div>
 </template>
@@ -15,4 +15,21 @@ export default {
 /* #app {
   중앙배치코드??
 } */
+
+body {
+  width: 100%;
+  height: 100%;
+  background-color: #e2e2e2;
+}
+
+.outside_box {
+  width: 35%;
+  height: 90%;
+  left: 50%;
+  top: 0;
+  position: absolute;
+  transform: translate(-50%, 0);
+  background-color: white;
+  border-radius: 15px;
+}
 </style>
