a082ce81c15c14237c3f968e75e2a80d8f41e8c4
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2023-05-07 18:04:56 +0900

    asd

--- src/components/ChatHead.vue
+++ src/components/ChatHead.vue
@@ -2,10 +2,16 @@
   <div class="chat_head">
     <div class="back_btn">&lt;
     </div>
-    <div class="opposit_box">허 은진님
-    </div>
+    <div class="opposit_box">허 은진님</div>
     <div class="pay_box">
-      <div class="pay_btn" @click="pay()">결제 참여</div>
+      <div class="pay_btn" @click="popup()">결제 참여</div>
+    </div>
+  </div>
+  <div class="popup_box" v-show="is_show">
+    <div class="popup_1">결제를 진행하시겠습니까?</div>
+    <div class="popup_2">
+      <div class="pay_sel" @click="pay()">네</div>
+      <div class="pay_sel2" @click="popup()">아니오</div>
     </div>
   </div>
 </template>
@@ -14,8 +20,16 @@
 export default {
   name: "ChatHead",
   methods: {
+    popup() {
+      this.is_show = !this.is_show;
+    },
     pay() {
       this.$router.push('/PayView');
+    }
+  },
+  data() {
+    return {
+      is_show: false
     }
   }
 };
@@ -66,5 +80,54 @@ export default {
   border-radius: 5px;
   font-size: 0.8rem;
   line-height: 280%;
+}
+
+.popup_box {
+  width: 70%;
+  height: 30%;
+  display: block;
+  margin: 0 auto;
+  top: 20%;
+  position: relative;
+  background-color: #F0F0F0;
+  border-radius: 15px;
+}
+
+.popup_1 {
+  width: 100%;
+  height: 70%;
+  text-align: center;
+  line-height: 900%;
+  position: relative;
+  font-size: 1.2rem;
+}
+
+.popup_2 {
+  width: 100%;
+  height: 30%;
+}
+
+.pay_sel {
+  width: 25%;
+  height: 40%;
+  float: left;
+  left: 20%;
+  position: relative;
+  background-color: #D9C696;
+  border-radius: 15px;
+  text-align: center; 
+  line-height: 200%;
+}
+
+.pay_sel2 {
+  width: 25%;
+  height: 40%;
+  float: left;
+  left: 30%;
+  position: relative;
+  background-color: #D9C696;
+  border-radius: 15px;
+  text-align: center; 
+  line-height: 200%;
 }
 </style>
