936fa9cab26e3aceb4007b2239cceb3d994b82d4
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2023-05-11 22:43:10 +0900

    Update ChatListItem.vue

--- src/components/ChatListItem.vue
+++ src/components/ChatListItem.vue
@@ -1,9 +1,80 @@
 <template>
-  <div>채팅 리스트 요소(채팅 목록 요소v-for,ul,li 활용)</div>
+  <div class="chatlistbox">채팅 리스트
+    <ul>
+      <li v-for="item in list" v-bind:key="item">
+        <div class="chatindex">
+          <img src="@/assets/chatlist_shop_icon.png" class="shop_icon">
+          <div class="chat_info">
+            <div class="info_line">
+              {{ item.at(0) }}
+            </div>
+            <div class="info_line">
+              {{ item.at(1) }}
+            </div>
+            <div class="info_line">
+              {{ item.at(2) }}
+            </div>
+          </div>
+        </div>
+        <hr>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: "ChatListItem",
+  data() {
+    return {
+      list: [['허은진님과의 대화방', '네, 그렇게 하시죠.', '장전초'],['김수진', 'ㄱㄱ', '금전초'],['고민석', 'ㄷㅊㄹ', '남일고']]
+    };
+  }
 };
 </script>
+
+<style scoped>
+.info_line {
+  height: 33.3%;
+  width: 100%;
+}
+.chat_info {
+  width: 78%;
+  height: 100%;
+  float: left;
+}
+
+
+.shop_icon {
+  width: 22%;
+  height: 100%;
+  float: left;
+}
+
+.chatlistbox {
+  width: 100%;
+  height: 86.5%;
+}
+
+.chatindex {
+  width: 100%;
+  height: 100%;
+  bottom: 5%;
+}
+
+ul {
+  list-style: none;
+}
+
+li {
+  left: -40px;
+  position: relative;
+  width: 100%;
+  height: 100px;
+}
+
+hr {
+  position: relative;
+  bottom: 10%;
+}
+</style>
