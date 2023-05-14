5e8abd3f5165c112ad28b3a6509a715abac79a31
chikako_2426 <78514962+kimikitae@users.noreply.github.com>
2023-05-14 21:24:46 +0900

    thank you suck!

--- src/components/HomePageItem.vue
+++ src/components/HomePageItem.vue
@@ -1,18 +1,110 @@
 <template>
-  <!-- <div>홈페이지 아이템 요소(게시글 목록 요소v-for,ul,li 활용)</div> -->
-  <hr class="first_line">
+  <div class="homelistbox">
+    <ul>
+      <template v-for="(item, index) in list" v-bind:key="index">
+        <li>
+          <div class="homeindex"  @click="viewpost()">
+            <img src="@/assets/chatlist_shop_icon.png" class="shop_icon" />
+            <div class="post_info">
+              <div class="info_line">
+                {{ item.title }}
+              </div>
+              <div class="info_line">
+                {{ item.where }}
+              </div>
+            </div>
+          </div>
+          <hr/>
+        </li>
+      </template>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HomePageItem",
+  data() {
+    return {
+      list: [
+        {
+          title: "자메이카 한주디 하실분",
+          where: "장전초",
+        },
+        {
+          title: "후라이드 노나묵자!",
+          where: "금전초",
+        },
+        {
+          title: "응애!",
+          where: "남일고",
+        },
+        {
+          title: "응애!",
+          where: "남일고",
+        },
+        {
+          title: "응애!",
+          where: "남일고",
+        },
+        {
+          title: "응애!",
+          where: "남일고",
+        },
+        {
+          title: "응애!",
+          where: "남일고",
+        },
+      ],
+    };
+  },
+  methods: {
+    viewpost() {
+      this.$router.push("/ViewPostView");
+    },
+  },
 };
 </script>
 
 <style scoped>
-.first_line{
+.homelistbox {
+  width: 100%;
+  height: 86.5%;
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+ul {
+  list-style: none;
+  padding-left: 0%;
+}
+li {
   position: relative;
-  /* top:2%; */
-  border: solid 0.4px rgb(208, 208, 208);
+  width: 100%;
+  height: 100px;
+}
+.homeindex {
+  width: 100%;
+  height: 86.5%;
+  bottom: 5%;
+}
+.shop_icon {
+  width: 22%;
+  height: 100%;
+  float: left;
+}
+.post_info {
+  width: 78%;
+  height: 100%;
+  float: left;
+}
+.info_line {
+  height: 50%;
+  width: 100%;
+}
+
+
+hr {
+  position: relative;
+  bottom: 10%;
 }
 </style>
