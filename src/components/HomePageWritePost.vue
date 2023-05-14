adda760841807751c8e4caff77bbda710cfe11be
chikako_2426 <78514962+kimikitae@users.noreply.github.com>
2023-05-14 20:26:51 +0900

    write post btn clear

--- src/components/HomePageWritePost.vue
+++ src/components/HomePageWritePost.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>글쓰기 버튼</div>
+  <img src="../assets/home_writepost_icon_btn.png" @click="writepost">
 </template>
 
 <script>
 export default {
   name: "HomePageWritePost",
+  data() {
+    return {
+      loc: "금정구 자취방"
+      
+    };
+  },
+  methods: {
+    writepost() {
+      this.$router.push('/WritePostView');
+    }
+  }
 };
 </script>
+
+<style scoped>
+img{
+  height: 6%;
+  width: 10%;
+  z-index: 3;
+  position: fixed;
+  right: 3%;
+  bottom: 3%;
+}
+</style>
