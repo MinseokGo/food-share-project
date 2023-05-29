785fbd63481d10ebb4284809304c70f17efac657
chikako_2426 <78514962+kimikitae@users.noreply.github.com>
2023-05-29 18:39:46 +0900

    ㄷㅈㅂ

--- src/http.js
+++ src/http.js
@@ -3,24 +3,22 @@ import login from "./store/login.js"
 
 const http = axios.create({
     baseURL: "http://localhost:3000",
-    headers: { "Content-Type" : "application/json",
-        Authorization: "",
-},
+    headers: { Authorization: "",
+    },
 })
 
 http.interceptors.request.use(
-  config => {
-    const isAuthenticated = login.state.isAuthenticated
-    if (isAuthenticated) {
-        config.headers.Authorization = login.state.token.accessToken
-    }
-    return config
-  },
-  error => {
-    // Do something with request error
+    config => {
+        const isAuthenticated = login.state.isAuthenticated
+        if (isAuthenticated) {
+            config.headers.Authorization = login.state.token.accessToken
+        }
+        return config
+    },
+    error => {
+        // Do something with request error
         Promise.reject(error)
-  }
+    }
 )
-// http.defaults.headers.post["Content-Type"] = "application/json"
 
 export default http
--- src/router.js
+++ src/router.js
@@ -1,4 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
+import login from "./store/login.js"
+
+
 
 import LoginView from "/src/views/LoginView.vue"
 import HomePageView from "/src/views/HomePageView.vue"
@@ -10,17 +13,32 @@ import ChatView from "/src/views/ChatView.vue"
 import ChatListView from "/src/views/ChatListView.vue"
 import PayView from "/src/views/PayView.vue"
 
+const beforeAuth = isAuth => (from, to, next) => {
+    const isAuthenticated = login.state.isAuthenticated
+    if ((isAuthenticated && isAuth) || (!isAuthenticated && !isAuth)) {
+        if(isAuth){
+            console.log("유효한 토큰 보유 및 인증 요구 페이지 접근")
+        } else{
+            console.log("토큰 없음 및 비인증 페이지 접근")
+        }
+      return next()
+    } else {
+      // 로그인 화면으로 이동
+      next("/")
+    }
+  }
+
 // 라우터 설계
 const routes = [
-        {path: "/", component: LoginView},
-        {path: "/HomePageView", component: HomePageView},
-        {path: "/SetLocView", component: SetLocView},
-        {path: "/UserInfoView", component: UserInfoView},
-        {path: "/ViewPostView", component: ViewPostView},
-        {path: "/WritePostView", component: WritePostView},
-        {path:"/ChatView", component: ChatView},
-        {path:"/ChatListView", component: ChatListView},
-        {path:"/PayView", component: PayView}
+        {path: "/", component: LoginView, },
+        {path: "/HomePageView", component: HomePageView, beforeEnter: beforeAuth(true),},
+        {path: "/SetLocView", component: SetLocView, beforeEnter: beforeAuth(true),},
+        {path: "/UserInfoView", component: UserInfoView, beforeEnter: beforeAuth(true),},
+        {path: "/ViewPostView", component: ViewPostView, beforeEnter: beforeAuth(true),},
+        {path: "/WritePostView", component: WritePostView, beforeEnter: beforeAuth(true),},
+        {path:"/ChatView", component: ChatView, beforeEnter: beforeAuth(true),},
+        {path:"/ChatListView", component: ChatListView, beforeEnter: beforeAuth(true),},
+        {path:"/PayView", component: PayView, beforeEnter: beforeAuth(true),}
 ]
 
 const router = createRouter({
